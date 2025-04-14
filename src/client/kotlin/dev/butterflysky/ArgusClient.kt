1ef921d862f6d7af479197f82c1c76f44d591072
butterflysky <bright.art1794@fastmail.com>
2025-04-13 19:40:22 -0700

    feat: import initial template files

---
+++ src/client/kotlin/dev/butterflysky/ArgusClient.kt
@@ -0,0 +1,9 @@
+package dev.butterflysky
+
+import net.fabricmc.api.ClientModInitializer
+
+object ArgusClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/dev/butterflysky/Argus.kt
@@ -0,0 +1,15 @@
+package dev.butterflysky
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object Argus : ModInitializer {
+    private val logger = LoggerFactory.getLogger("argus")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
